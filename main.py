6133e9d4330f65939d79e3c5fd2ede165c5db4ca
Brian Tang <ashrielbrian@gmail.com>
2021-07-27 19:12:28 +0800

    Completes random forest training module

--- main.py
+++ main.py
@@ -99,11 +99,20 @@ def go(config: DictConfig):
             # NOTE: use the rf_config we just created as the rf_config parameter for the train_random_forest
             # step
 
-            ##################
-            # Implement here #
-            ##################
+            _ = mlflow.run(
+                os.path.join(hydra_root, 'src', 'train_random_forest'),
+                'main',
+                parameters={
+                    'trainval_artifact': f"{config['main']['project_name']}/trainval_data.csv:latest",
+                    'val_size': config['modeling']['val_size'],
+                    'random_seed': config['modeling']['random_seed'],
+                    'stratify_by': config['modeling']['stratify_by'],
+                    'rf_config': rf_config,
+                    'max_tfidf_features': config['modeling']['max_tfidf_features'],
+                    'output_artifact': 'random_forest_export',
+                }
+            )
 
-            pass
 
         if "test_regression_model" in active_steps:
 
